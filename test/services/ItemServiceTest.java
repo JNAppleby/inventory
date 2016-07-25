0d1eb12238ac8d81eba3acaf1406c051254901bf
Yaroslav Mudryy <ymudryy@sofi.org>
2016-07-25 14:34:28 -0600

    JUnit tests for equals() of the Item entity

--- test/services/ItemServiceTest.java
+++ test/services/ItemServiceTest.java
@@ -1,5 +1,7 @@
 package services;
 
+import errcodes.ErrCode;
+
 import conf.AppConf;
 import conf.TestDataConf;
 import entities.Item;
@@ -12,9 +14,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-@ContextConfiguration(classes = {
-    AppConf.class, TestDataConf.class
-})
+@ContextConfiguration(classes = {AppConf.class, TestDataConf.class})
 public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
 
     @Inject private ItemService itemSrv;
@@ -27,6 +27,7 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         List<Item> items = itemSrv.getAllItems();
         Assert.assertNotNull("List of items is null", items);
         Assert.assertEquals("Wrong size of the list, should be 0", 0, items.size());
+        Assert.assertTrue("Items list should be empty", items.isEmpty());
     }
 
     /**
@@ -40,14 +41,13 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
         Item item1 = new Item("item1", "item1 description");
-        itemSrv.addItem(item1);
+        Assert.assertEquals("Item adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item1));
         items = itemSrv.getAllItems();
 
         Item retrievedItem = items.get(0);
 
         Assert.assertEquals("Total item count should be 1", 1, items.size());
         Assert.assertNotNull("Item retrieved is null", retrievedItem);
-        Assert.assertNotNull("Item Name of retrieved element is null", retrievedItem.getName());
     }
 
     /**
@@ -61,13 +61,13 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
         Item item = new Item("item1", "item1 description");
-        itemSrv.addItem(item);
+        Assert.assertEquals("Item 1 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 1", 1, items.size());
 
         item = new Item("item2", "item2 description");
-        itemSrv.addItem(item);
+        Assert.assertEquals("Item 2 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 2", 2, items.size());
@@ -76,8 +76,6 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Item retrievedItem2 = items.get(1);
 
         Assert.assertNotEquals("Items in the list are not unique, but should have been.", retrievedItem1, retrievedItem2);
-        Assert.assertNotEquals("Names of the items in the list are not unique, but should have been.", retrievedItem1.getName(),
-                               retrievedItem2.getName());
     }
 
     /**
@@ -91,42 +89,61 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
         Item item = new Item("item1", "item1 description");
-        itemSrv.addItem(item);
+        Assert.assertEquals("Item adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 1", 1, items.size());
 
         Item retrievedItem = items.get(0);
-
         Assert.assertEquals("Retrieved item is not the same as original", item, retrievedItem);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item1", retrievedItem.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item1 description", retrievedItem.getDesc());
     }
 
     /**
-     * Add duplicate item
+     * Add same/exact duplicate item
      */
     @Test
-    public void addDuplicateItemTest() {
+    public void addExactDupItemTest() {
         List<Item> items = itemSrv.getAllItems();
         Assert.assertNotNull("List of items is null", items);
 
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
         Item item = new Item("item1", "item1 description");
-        itemSrv.addItem(item);
+        Assert.assertEquals("Item adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 1", 1, items.size());
 
-        // Add same exact/original/duiplicate item
-        itemSrv.addItem(item);
+        // Add same exact/original/duplicate item
+        Assert.assertEquals("Duplicate item protection fails", ErrCode.ADD_DUPLICATE_ITEM, itemSrv.addItem(item));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should still be 1 after adding duplicate item", 1, items.size());
 
+        Item retrievedItem = items.get(0);
+
+        Assert.assertEquals("Retrieved item is not the same as original", item, retrievedItem);
+    }
+
+    /**
+     * Add different item, but with duplicate Name
+     */
+    @Test
+    public void addDiffItemDupNameTest() {
+        final String itemName = "item1";
+        List<Item> items = itemSrv.getAllItems();
+        Assert.assertNotNull("List of items is null", items);
+
+        Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
+
+        Item item = new Item(itemName, "item1 description");
+        Assert.assertEquals("Item adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item));
+
+        items = itemSrv.getAllItems();
+        Assert.assertEquals("Total item count should be 1", 1, items.size());
+
         // Add different item, but with duplicate Name
-        Item diffItemSameName = new Item("item1", "different description");
+        Item diffItemSameName = new Item(itemName, "different description");
         itemSrv.addItem(diffItemSameName);
 
         items = itemSrv.getAllItems();
@@ -137,8 +154,6 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Retrieved item is not the same as original", item, retrievedItem);
         Assert.assertNotEquals("Retrieved item should NOT be the same as duplicate item that was attempted to be inserted",
                                diffItemSameName, retrievedItem);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item1", retrievedItem.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item1 description", retrievedItem.getDesc());
     }
 
     /**
@@ -148,10 +163,9 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
     public void addNullItemTest() {
         List<Item> items = itemSrv.getAllItems();
         Assert.assertNotNull("List of items is null", items);
-
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
-        itemSrv.addItem(null);
+        Assert.assertEquals("ADD_NULL_ITEM error should have been returned", ErrCode.ADD_NULL_ITEM, itemSrv.addItem(null));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be still 0", 0, items.size());
@@ -169,14 +183,14 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, initialCount);
 
         Item item1 = new Item("item1", "item1 description");
-        itemSrv.addItem(item1);
+        Assert.assertEquals("Item 1 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item1));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 1", 1, items.size());
 
         // Add second item
         Item item2 = new Item("item2", "item2 description");
-        itemSrv.addItem(item2);
+        Assert.assertEquals("Item 2 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item2));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 2", 2, items.size());
@@ -185,12 +199,7 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Item retrievedItem2 = items.get(1);
 
         Assert.assertEquals("Retrieved item1 is not the same as original", item1, retrievedItem1);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item1", retrievedItem1.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item1 description", retrievedItem1.getDesc());
-
         Assert.assertEquals("Retrieved item2 is not the same as original", item2, retrievedItem2);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item2", retrievedItem2.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item2 description", retrievedItem2.getDesc());
     }
 
     /**
@@ -204,17 +213,15 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
         Item item1 = new Item("item1", "item1 description");
-        itemSrv.addItem(item1);
+        Assert.assertEquals("Item 1 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item1));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("One item added, total item count should be 1 now", 1, items.size());
 
         Item retrievedItem1 = items.get(0);
         Assert.assertEquals("Retrieved item1 is not the same as original", item1, retrievedItem1);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item1", retrievedItem1.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item1 description", retrievedItem1.getDesc());
 
-        itemSrv.removeItemById(retrievedItem1.getId());
+        Assert.assertEquals("Error removing an item", ErrCode.REMOVE_SUCCESS, itemSrv.removeItemById(retrievedItem1.getId()));
         items = itemSrv.getAllItems();
         Assert.assertEquals("Item was removed, count should be back to 0", 0, items.size());
     }
@@ -229,9 +236,24 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
 
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
-        itemSrv.removeItemById(-42L);
+        Assert.assertEquals("Should have returned REMOVE_NON_EXISTENT code", ErrCode.REMOVE_NON_EXISTENT, itemSrv.removeItemById(42L));
         items = itemSrv.getAllItems();
         Assert.assertEquals("Non-existing item deletion was attempted, count should be still 0", 0, items.size());
+    }
+
+    /**
+     * Remove item with negative Id
+     */
+    @Test
+    public void removeNegativeIdItemTest() {
+        List<Item> items = itemSrv.getAllItems();
+        Assert.assertNotNull("List of items is null", items);
+
+        Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
+
+        Assert.assertEquals("Should have returned REMOVE_NON_EXISTENT code", ErrCode.REMOVE_NON_EXISTENT, itemSrv.removeItemById(-1L));
+        items = itemSrv.getAllItems();
+        Assert.assertEquals("Negative and non-existing item deletion was attempted, count should be still 0", 0, items.size());
     }
 
     /**
@@ -245,22 +267,20 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, items.size());
 
         Item item1 = new Item("item1", "item1 description");
-        itemSrv.addItem(item1);
+        Assert.assertEquals("Item 1 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item1));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("One item added, total item count should be 1 now", 1, items.size());
 
         Item retrievedItem1 = items.get(0);
         Assert.assertEquals("Retrieved item1 is not the same as original", item1, retrievedItem1);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item1", retrievedItem1.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item1 description", retrievedItem1.getDesc());
 
-        itemSrv.removeItemById(retrievedItem1.getId());
+        Assert.assertEquals("Error removing an item", ErrCode.REMOVE_SUCCESS, itemSrv.removeItemById(retrievedItem1.getId()));
         items = itemSrv.getAllItems();
         Assert.assertEquals("Item was removed, count should be back to 0", 0, items.size());
 
         // Removing the same exact item
-        itemSrv.removeItemById(retrievedItem1.getId());
+        Assert.assertEquals("Should return REMOVE_NON_EXISTENT code", ErrCode.REMOVE_NON_EXISTENT, itemSrv.removeItemById(retrievedItem1.getId()));
         items = itemSrv.getAllItems();
         Assert.assertEquals("Items count should be still 0 after removing the only item twice in the row", 0, items.size());
     }
@@ -277,14 +297,14 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Assert.assertEquals("Wrong initial size of the list, should be 0", 0, initialCount);
 
         Item item1 = new Item("item1", "item1 description");
-        itemSrv.addItem(item1);
+        Assert.assertEquals("Item 1 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item1));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 1", 1, items.size());
 
         // Add second item
         Item item2 = new Item("item2", "item2 description");
-        itemSrv.addItem(item2);
+        Assert.assertEquals("Item 2 adding failure", ErrCode.ADD_SUCCESS, itemSrv.addItem(item2));
 
         items = itemSrv.getAllItems();
         Assert.assertEquals("Total item count should be 2", 2, items.size());
@@ -293,26 +313,19 @@ public class ItemServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         Item retrievedItem2 = items.get(1);
 
         Assert.assertEquals("Retrieved item1 is not the same as original", item1, retrievedItem1);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item1", retrievedItem1.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item1 description", retrievedItem1.getDesc());
-
         Assert.assertEquals("Retrieved item2 is not the same as original", item2, retrievedItem2);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item2", retrievedItem2.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item2 description", retrievedItem2.getDesc());
 
         // Remove first item
-        itemSrv.removeItemById(retrievedItem1.getId());
+        Assert.assertEquals("Error removing 1st item", ErrCode.REMOVE_SUCCESS, itemSrv.removeItemById(retrievedItem1.getId()));
         items = itemSrv.getAllItems();
         Assert.assertEquals("One of two items was removed, count should be 1", 1, items.size());
 
         Item lastRemainingItem = items.get(0);
         // lastRemainingItem should be the second inserted item
         Assert.assertEquals("Retrieved item2 is not the same as original", item2, lastRemainingItem);
-        Assert.assertEquals("Retrieved item's Name is not the same as saved", "item2", lastRemainingItem.getName());
-        Assert.assertEquals("Retrieved item's Description is not the same as saved", "item2 description", lastRemainingItem.getDesc());
 
         // Remove second item
-        itemSrv.removeItemById(retrievedItem2.getId());
+        Assert.assertEquals("Error removing 2nd item", ErrCode.REMOVE_SUCCESS, itemSrv.removeItemById(retrievedItem2.getId()));
         items = itemSrv.getAllItems();
         Assert.assertEquals("Last item was removed from db, count should be 0", 0, items.size());
     }
---
+++ test/services/impl/ItemServiceImplTest.java
@@ -0,0 +1,66 @@
+package services.impl;
+
+import errcodes.ErrCode;
+
+import conf.AppConf;
+import conf.TestDataConf;
+import entities.Item;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+@ContextConfiguration(classes = {AppConf.class, TestDataConf.class})
+public class ItemServiceImplTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    /**
+     * Test equals with exact same item
+     */
+    @Test
+    public void equalsSameExactItemTest() {
+        Item item1 = new Item("item1", "item1 description");
+        Assert.assertEquals("Items are not the same", item1, item1);
+    }
+
+    /**
+     * Test equals with equivalent items
+     */
+    @Test
+    public void equalsEquivalentItemsTest() {
+        final String itemName = "item1";
+        final String itemDesc = "item1 description";
+
+        Item item1 = new Item(itemName, itemDesc);
+        Item item2 = new Item(itemName, itemDesc);
+
+        Assert.assertEquals("Items are not the same", item1, item2);
+    }
+
+    /**
+     * Test equals with different items
+     */
+    @Test
+    public void equalsDiffItemsTest() {
+        Item item1 = new Item("item1", "item1 desc");
+        Item item2 = new Item("item2", "item2 desc");
+
+        Assert.assertNotEquals("Items are evaluated to be the same, while they are not", item1, item2);
+    }
+
+    /**
+     * Test equals with null description
+     */
+    @Test
+    public void equalsNullDescTest() {
+        final String itemName = "item1";
+
+        Item item1 = new Item(itemName, null);
+        Item item2 = new Item(itemName, null);
+
+        Assert.assertEquals("Items are not the same", item1, item2);
+    }
+}
